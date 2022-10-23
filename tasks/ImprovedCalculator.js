5bba85dbf7dec5cd2e0a36ec44a966a642f0d51e
vakulenk0 <113202090+vakulenk0@users.noreply.github.com>
2022-10-23 14:07:41 +0300

    Create ImprovedCalculator.js

---
+++ tasks/ImprovedCalculator.js
@@ -0,0 +1,87 @@
+window.addEventListener("DOMContentLoaded", function() {
+    let sel = document.getElementById("select");
+    let result = document.getElementById("result");
+    let auto = document.getElementById("block-radios");
+    let phone = document.getElementById("block-ckeckbox");
+    auto.style.display = "none";
+    sel.addEventListener("change", Change);
+    result.innerHTML = "20000";
+    let optionsOfPhone = document.getElementsByName("check");
+    optionsOfPhone.forEach(function (element){element.flag = 0;});
+    let ChooseAuto = document.getElementsByName("radio");
+    ChooseAuto.forEach(function (element){element.checked = false;});
+    let flagAuto = 0, flagPhone = 0, flag = 0;
+    let bufer = 0;
+    let field = document.getElementsByName("field1");
+    field[0].value = "1";
+    Change();
+
+    function Change(){
+        if(sel.value === "20000"){
+            auto.style.display = "none";
+            phone.style.display = "block";
+            result.innerHTML = "20000";
+            optionsOfPhone.forEach(function (element){
+                element.flag = 0; element.checked = false;
+            });
+            field[0].value = "1";
+            bufer = 20000;
+        }
+        if(sel.value === ""){
+            phone.style.display = "none";
+            auto.style.display = "block";
+            result.innerHTML = "";
+            ChooseAuto.forEach(function (element)
+            {element.checked = false;
+            });
+            field[0].value = "1";
+            bufer = 0;
+        }
+        if(sel.value === "5"){
+            phone.style.display = "none";
+            auto.style.display = "none";
+            result.innerHTML = "5";
+            field[0].value = "1";
+            bufer = 5;
+        }
+    }
+    optionsOfPhone.forEach(function (element){
+        element.addEventListener("change", function (){
+            if(element.flag === 0){
+                bufer += parseInt(element.value);
+                let temp = parseInt(field[0].value);
+                result.innerHTML = (bufer*temp).toString();
+                element.flag = 1;
+            }
+            else{
+                bufer -= parseInt(element.value);
+                let temp = parseInt(field[0].value);
+                result.innerHTML = (bufer*temp).toString();
+                element.flag = 0;
+            }
+        });
+    });
+
+    ChooseAuto.forEach(function(element){
+        element.addEventListener("change", function (){
+            result.innerHTML = element.value;
+            bufer = parseInt(element.value);
+        })
+    });
+
+    let button = document.getElementById("button1");
+    button.addEventListener("click", calcCost);
+    function calcCost() {
+        let a = field[0].value;
+        const obj = new RegExp("[^0-9]");
+        if (obj.test(a) === true || a === "") {
+            alert("Проверьте введённые данные!");
+        }
+        else {
+            result.innerHTML = (parseInt(a)*bufer).toString();
+        }
+    }
+
+    let c = document.getElementById("button1");
+    c.addEventListener("click", calcCost);
+});
